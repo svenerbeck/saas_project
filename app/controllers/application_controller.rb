1c5b966c51ccb7ca3ea7867766d3737a638ca795
Sven Erbeck <sven.erbeck@datarhei.org>
2018-04-17 09:19:13 +0000

    Updated flash message styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+  # Whitelist the following form fields that we can process them, if coming from 
+  # a Devise signup form
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # Whitelist the following form fields that we can process them
+  
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:stripe_card_token, :email, :password, :password_confirmation) }
